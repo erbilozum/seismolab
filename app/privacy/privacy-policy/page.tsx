ae330302552d1ac0bc1e9801712e281f16c31a9a
Erbil Özum <erbilozum@gmail.com>
2026-02-13 13:43:31 +0200

    google search

--- app/privacy/privacy-policy/page.tsx
+++ app/privacy/privacy-policy/page.tsx
@@ -2,22 +2,22 @@
 import {motion} from 'framer-motion';
 import React from 'react';
 import '../privacs.css'
-import TitleCardSmall from "@/components/cards/titleCardSmall";
+import TitleSmallCard from "@/components/card/titleSmallCard";
 
 const Page = () => {
     return (
         <div className="min-h-screen w-screen flex font-montserrat text-gray-700 mt-12">
             <motion.div initial={{opacity: 0}} whileInView={{opacity: 1}} className={'mx-auto max-w-7xl mt-12 h-full'}>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Gizlilik Politikası"}/>
+                    <TitleSmallCard title={"Gizlilik Politikası"}/>
                 </div>
                 <p>Bu İnternet sitesini kullanarak kişisel verilerinizin işlenmesini kabul etmiş olursunuz. Güvenliğiniz bizim için önemli. Bu sebeple, bizimle paylaşacağınız kişisel verileriniz hassasiyetle korunmaktadır.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Veri Sorumlusu"}/>
+                    <TitleSmallCard title={"Veri Sorumlusu"}/>
                 </div>
                 <p>Veri sorumlusu olarak, bu gizlilik ve kişisel verilerin korunması politikası ile ziyaret etmekte olduğunuz İnternet sitesi kapsamında hangi kişisel verilerinizin hangi amaçlarla işleneceği, işlenen verilerin kimlerle ve hangi sebeplerle paylaşılabileceği, veri işleme yöntemimiz ve hukuki sebepleri ile; işlenen verilerinize ilişkin haklarınızın neler olduğu hususunda siz kullanıcılarımızı aydınlatmayı amaçlıyorum.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Toplanan Kişisel Veriler"}/>
+                    <TitleSmallCard title={"Toplanan Kişisel Veriler"}/>
                 </div>
                 <p>Bu İnternet sitesi tarafından toplanan kişisel verileriniz: </p>
                 <ul>
@@ -25,24 +25,24 @@ const Page = () => {
                     <li>Konum bilgileri</li>
                 </ul>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Kullanılan Servisler"}/>
+                    <TitleSmallCard title={"Kullanılan Servisler"}/>
                 </div>
                 <h3>Analitik ve izleme</h3>
                 <ul>
                     <li><strong>Google Analytics</strong>: Google Analytics, ziyaretçi davranışlarını ve site kullanımını analiz etmek için kullanılmaktadır. Bu hizmet, ziyaretçi trafiği, etkileşimler ve kullanıcı davranışları hakkında detaylı raporlar sağlar.</li>
                 </ul>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Verilerin İşlenme Amaçları"}/>
+                    <TitleSmallCard title={"Verilerin İşlenme Amaçları"}/>
                 </div>
                 <p>Kişisel verileriniz, bu İnternet sitesi tarafından amacına uygun hizmet sunulabilmesi, yasal yükümlülüklerin yerine getirilmesi, hizmet kalitesinin artırılması, iletişim, güvenlik ve gerektiğinde yasal merciler ile bilgi paylaşılabilmesi amaçları ile işlenmektedir. Kişisel verileriniz, bu sayılan amaçlar dışında kullanılmayacaktır.</p>
                 <h2>Verilerin Aktarılması</h2>
                 <p>Bu İnternet sitesi tarafından toplanan kişisel verileriniz, yasal zorunluluklar haricinde, açık rızanız olmadan üçüncü kişiler ile paylaşılmaz. Ancak hizmet sağlayıcılarımız, iş ortaklarımız ve yasal merciler ile, hizmetin sağlanması ve yasal yükümlülüklerin yerine getirilmesi amaçlarıyla gerekli olduğu ölçüde paylaşılabilir.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Çerez Kullanımı"}/>
+                    <TitleSmallCard title={"Çerez Kullanımı"}/>
                 </div>
                 <p>Bu İnternet sitesi çerez kullanmaktadır. Çerezler, bir İnternet sayfası ziyaret edildiğinde kullanıcılara ilişkin birtakım bilgilerin kullanıcıların terminal cihazlarında depolanmasına izin veren düşük boyutlu zengin metin biçimli text formatlarıdır. Çerezler, bir İnternet sitesini ilk ziyaretiniz sırasında tarayıcınız aracılığıyla cihazınıza depolanabilir ve aynı siteyi aynı cihazla tekrar ziyaret ettiğinizde, tarayıcınız cihazınızda site adına kayıtlı bir çerez olup olmadığını kontrol eder. Eğer kayıt var ise, kaydın içindeki veriyi ziyaret etmekte olduğunuz İnternet sitesine iletir. Bu sayede İnternet sitesi, sizin daha önceki ziyaretinizi tespit eder ve size iletilecek içeriği ona göre belirler.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Yasal Haklarınız"}/>
+                    <TitleSmallCard title={"Yasal Haklarınız"}/>
                 </div>
                 <h3>KVKK Kapsamında Haklarınız</h3>
                 <p>6698 sayılı KVKK madde 11 uyarınca herkes, veri sorumlusuna başvurarak kendisiyle ilgili aşağıda yer alan taleplerde bulunma hakkına sahiptir:</p>
@@ -58,11 +58,11 @@ const Page = () => {
                     <li>Kişisel verilerin kanuna aykırı olarak işlenmesi sebebiyle zarara uğraması hâlinde zararın giderilmesini talep etme.</li>
                 </ol>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"İletişim"}/>
+                    <TitleSmallCard title={"İletişim"}/>
                 </div>
                 <p>Kişisel verilerinizle ilgili haklarınızı kullanmak veya Gizlilik Politika&apos;mız hakkında daha fazla bilgi almak için ts.junior.dev@gmail.com adresinden bizimle iletişime geçebilirsiniz.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Onay ve Yükümlülük"}/>
+                    <TitleSmallCard title={"Onay ve Yükümlülük"}/>
                 </div>
                 <p>İnternet sitemiz ile kişisel verilerinizi paylaşmak, tamamen sizin tercihinizdir. İnternet sitemizi kullanmaya devam ettiğiniz takdirde, bu Gizlilik Politikası&apos;nı kabul ettiğiniz varsayılacaktır. Bu politika, <strong>1 Şubat 2026</strong> tarihinde yürürlüğe girmiş olup, gerektiğinde güncellenir.</p>
             </motion.div>
--- app/privacy/terms-and-conditions/page.tsx
+++ app/privacy/terms-and-conditions/page.tsx
@@ -2,25 +2,25 @@
 import {motion} from 'framer-motion';
 import React from 'react';
 import '../privacs.css'
-import TitleCardSmall from "@/components/cards/titleCardSmall";
+import TitleSmallCard from "@/components/card/titleSmallCard";
 const Page = () => {
     return (
         <div className="min-h-screen w-screen flex font-montserrat text-gray-700 mt-12">
             <motion.div initial={{opacity: 0}} whileInView={{opacity: 1}} className={'mx-auto max-w-7xl mt-12 h-full'}>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Kullanım Koşulları"}/>
+                    <TitleSmallCard title={"Kullanım Koşulları"}/>
                 </div>
                 <p>Bu Kullanım Koşulları, bu internet sitesini kullanırken uymanız gereken kuralları içermektedir. Lütfen İnternet sitesini kullanmaya devam etmeden önce bu koşulları dikkatli bir şekilde okuyunuz.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Kapsam ve Değişiklikler"}/>
+                    <TitleSmallCard title={"Kapsam ve Değişiklikler"}/>
                 </div>
                 <p> Bu Kullanım Koşulları, internet sitesi kullanımı sırasında uygulanacak kuralları ve tarafların hak ve yükümlülüklerini belirlemektedir. İnternet sitesini kullanmaya devam ettiğiniz takdirde bu koşulları kabul ettiğini varsayılacaktır.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Hizmet İçeriği ve Kullanım"}/>
+                    <TitleSmallCard title={"Hizmet İçeriği ve Kullanım"}/>
                 </div>
                 <p>Bu İnternet sitesi tarafından sunulan hizmetler, yasal mevzuatlara uygun bir şekilde kullanılmak üzere tasarlanmıştır. Kullanıcılar, İnternet sitesini kullanırken tüm yasa, tüzük ve yönetmelikler ile uluslararası sözleşmelere ve teamüllere uymakla yükümlüdür. Yazılımcı, herhangi bir zamanda, herhangi bir nedenle, önceden bildirimde bulunmaksızın hizmetlerini değiştirme, askıya alma veya sonlandırma hakkını saklı tutar.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Kullanıcı Yükümlülükleri"}/>
+                    <TitleSmallCard title={"Kullanıcı Yükümlülükleri"}/>
                 </div>
                 <p>Bu İnternet sitesinin kullanıcıları aşağıda yer alan kurallara uymakla yükümlüdür:</p>
                 <ol>
@@ -31,15 +31,15 @@ const Page = () => {
                     <li>Diğer kullanıcıların sunulan hizmetlerden yararlanmalarını engelleyecek nitelikte davranışlarda bulunmamak.</li>
                 </ol>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Gizlilik Politikası"}/>
+                    <TitleSmallCard title={"Gizlilik Politikası"}/>
                 </div>
                 <p>Bu İnternet sitesini kullanmaya devam etmekle Gizlilik Politika&apos;mızı kabul etmiş olursunuz. Kişisel verilerinizin nasıl toplandığı, kullanıldığı ve korunduğu hakkında detaylı bilgi için lütfen Gizlilik Politika&apos;mızı inceleyin.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Fikri Mülkiyet Hakları"}/>
+                    <TitleSmallCard title={"Fikri Mülkiyet Hakları"}/>
                 </div>
                 <p>İnternet sitesinde yer alan tüm içerik, tasarım, logo, yazılım ve sair materyaller, yazılımcı fikri mülkiyet ve telif haklarına haiz olup, fikir ve sanat eserleri kanunu ve sair yasal mevzuatlarca korunmaktadır. Bu içeriklerin izinsiz kullanımı, kopyalanması, çoğaltılması, değiştirilmesi veya dağıtılması yasaktır.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Genel Hükümler"}/>
+                    <TitleSmallCard title={"Genel Hükümler"}/>
                 </div>
                 <ol>
                     <li>Kullanıcıların tamamı, İnternet sitesini kullanmaya devam etmekle işbu platformu, hukuka ve ahlaka uygun bir şekilde ve yalnızca kişisel amaçlar ile sınırlı olmak üzere kullanacaklarını ve üçüncü kişinin haklarını ihlal eder nitelikte eylemlerde bulunmayacaklarını kabul ve taahhüt ederler.</li>
@@ -49,15 +49,15 @@ const Page = () => {
                     <li>İnternet sitesini virüslerden arındırılmış olarak tutmak konusunda elimizden geleni yapsak da, virüslerin tamamen bulunmadığı garantisini vermemekteyiz. Bu nedenle veri indirirken, virüslere karşı gerekli önlemi almak, kullanıcıların sorumluluğundadır.</li>
                 </ol>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Sorumluluk Sınırları"}/>
+                    <TitleSmallCard title={"Sorumluluk Sınırları"}/>
                 </div>
                 <p>İnternet sitesinin kullanımından doğabilecek doğrudan, dolaylı, özel veya cezai hiçbir zarardan sorumlu tutulamaz. İnternet sitemiz, &quot;olduğu gibi&quot; sunulmaktadır ve kesintisiz veya hatasız çalışacağına dair herhangi bir garanti verilmemektedir.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Uyuşmazlık Çözümü"}/>
+                    <TitleSmallCard title={"Uyuşmazlık Çözümü"}/>
                 </div>
                 <p>Bu İnternet sitesinin kullanımından doğabilecek uyuşmazlıklarda, yazılımcı hiç bir sorumluluk kabul etmemekte olup tüm sorumluluk kullanıcıya ait olacaktır. Bu koşullar, Türkiye kanunlarına göre yorumlanacak ve uygulanacaktır.</p>
                 <div className={"mt-4 mb-2"}>
-                    <TitleCardSmall title={"Son Hükümler"}/>
+                    <TitleSmallCard title={"Son Hükümler"}/>
                 </div>
                 <p>Bu Kullanım Koşulları, <strong>1 Şubat 2026</strong> tarihinde yürürlüğe girmiş olup, İnternet sitesini kullanmaya devam ettiğiniz takdirde işbu Kullanım Koşulları&apos;nı kabul ettiğiniz varsayılacaktır. Kullanım Koşulları içerisinde yer alan herhangi bir hükmün geçersiz sayılması, diğer hükümlerin geçerliliğini etkilemez.</p>
             </motion.div>
